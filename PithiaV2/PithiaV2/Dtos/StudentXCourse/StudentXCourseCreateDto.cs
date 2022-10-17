6535fcd8db0dc9b2656e9b0e6b5553cd9d82c4a9
jimtete <tetepoulidis@ieee.org>
2022-10-17 14:51:02 +0300

    refactoring

--- PithiaV2/PithiaV2/Dtos/StudentXCourse/StudentXCourseCreateDto.cs
+++ PithiaV2/PithiaV2/Dtos/StudentXCourse/StudentXCourseCreateDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PithiaV2.Dtos.StudentXCourse;
 
-public class SudentXCourseCreateDto
+public class StudentXCourseCreateDto
 {
     [Required] public Models.User User { get; set; }
     [Required] public Models.Course Course { get; set; }
