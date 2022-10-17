6ed596627463f0f33940aea028c080b99e324ade
jimtete <tetepoulidis@ieee.org>
2022-10-17 15:47:46 +0300

    Adding endpoint manager

---
+++ PithiaV2/PithiaV2/EndpointManager/EndpointDefinition.cs
@@ -0,0 +1,38 @@
+namespace PithiaV2.EndpointManager;
+
+public static class EndpointDefinition
+{
+    public static void AddEndpointDefinitions(
+        this IServiceCollection services, params Type[] scanMarkers)
+    {
+        var endpointDefinitions = new List<IEndpointDefinition>();
+
+        foreach (var marker in scanMarkers)
+        {
+            endpointDefinitions.AddRange(
+                marker.Assembly.ExportedTypes
+                    .Where(x => typeof(IEndpointDefinition).IsAssignableFrom(x)&& !x.IsClass)
+                    .Select(Activator.CreateInstance).Cast<IEndpointDefinition>()
+                );
+        }
+
+        foreach (var endpointDefinition in endpointDefinitions)
+        {
+            endpointDefinition.DefineServices(services);
+        }
+
+        services.AddSingleton(endpointDefinitions as IReadOnlyCollection<IEndpointDefinition>);
+    }
+
+    public static void UseEndpointDefinitions(this WebApplication app)
+    {
+        var definitions = app.Services.GetRequiredService<IReadOnlyCollection<IEndpointDefinition>>();
+
+        foreach (var endpointDefinition in definitions)
+        {
+            endpointDefinition.DefineEndpoints(app);
+        }
+    }
+    
+    
+}
