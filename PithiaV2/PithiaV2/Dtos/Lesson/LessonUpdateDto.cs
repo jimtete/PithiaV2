167311ef3a7dbb8a3c21adad34abd4beb370b2f5
jimtete <tetepoulidis@ieee.org>
2022-10-19 15:28:58 +0300

    Finishing with lesson-endpoints

--- PithiaV2/PithiaV2/Dtos/Lesson/LessonUpdateDto.cs
+++ PithiaV2/PithiaV2/Dtos/Lesson/LessonUpdateDto.cs
@@ -2,15 +2,123 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PithiaV2.Dtos.Lesson;
 
-public class LessonUpdateDto
+public class LessonUpdateDto : IValidatableObject
 {
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Day != null)
+        {
+            bool ValidDay;
+            switch (Day.ToLower())
+            {
+                case "monday" :
+                    ValidDay = true;
+                    break;
+                case "tuesday" :
+                    ValidDay = true;
+                    break;
+                case "wednesday" :
+                    ValidDay = true;
+                    break;
+                case "thursday" :
+                    ValidDay = true;
+                    break;
+                case "friday" :
+                    ValidDay = true;
+                    break;
+                case "saturday" :
+                    ValidDay = true;
+                    break;
+                default:
+                    ValidDay = false;
+                    break;
+            }
+
+            if (!ValidDay)
+            {
+                yield return new ValidationResult($"Your day is invalid, you gave me {Day} \n");
+            }
+        }
+
+        if (Minutes != null)
+        {
+            if (Minutes < 0)
+            {
+                yield return new ValidationResult("Minutes field cannot be a negative number \n");
+            }
+        }
+
+        if (Hours != null)
+        {
+            if (Hours < 0)
+            {
+                yield return new ValidationResult("Hours field cannot be a negative number \n");
+            }
+        }
+
+        if (Hours != null && Minutes != null)
+        {
+            int TotalTime = Hours * 60 + Minutes;
+
+            if (TotalTime == 0)
+            {
+                yield return new ValidationResult("Incorrect amount of lesson's time!\n");
+            }
+        }
+
+        // Time validation Chapter
+        if (Time != null)
+        {
+            //Checking the length
+            if (Time.Length != 5)
+            {
+                yield return new ValidationResult($"Incorrect time value, you gave me {Time}\n");
+            }
+        
+            //Checking the colon
+            if (!Time.Substring(2, 1).Equals(":"))
+            {
+                yield return new ValidationResult($"Character at index 2 must be :, you gave me {Time.Substring(2, 1)}\n");
+            }
+        
+            //Checking if Parseable
+            string hours = Time.Substring(0, 2);
+            string minutes = Time.Substring(3, 2);
+
+            int parseHours, parseMinutes;
+
+            if (!int.TryParse(hours, out parseHours))
+            {
+                yield return new ValidationResult($"{hours} is not parsable\n");
+            }
+
+            if (!int.TryParse(minutes, out parseMinutes))
+            {
+                yield return new ValidationResult($"{minutes} is not parsable\n");
+            }
+
+            if (parseHours < 0 || parseHours > 23)
+            {
+                yield return new ValidationResult($"{parseHours} not a valid hours value! \n");
+            }
+
+            if (parseMinutes < 0 || parseMinutes > 59)
+            {
+                yield return new ValidationResult($"{parseMinutes} not a valid minutes value! \n");
+            }
+        }
+    }
+
+
     [Key]
     public int Id { get; set; }
 
     public string? Day { get; set; }
 
+    [Required]
     public int Hours { get; set; }
 
+    [Required]
     public int Minutes { get; set; }
 
     public string? Time { get; set; }
--- PithiaV2/PithiaV2/Endpoints/LessonEndpoints.cs
+++ PithiaV2/PithiaV2/Endpoints/LessonEndpoints.cs
@@ -46,9 +46,85 @@ public class LessonEndpoints : IEndpointDefinition
         var results = mapper.Map<LessonReadDto>(lessonModel);
         return Results.Created($"/lessons/{results.Id}",results);
     }
+
+    internal async Task<IResult> GetAllLessons(ILessonRepo repo, IMapper mapper)
+    {
+        var lessons = await repo.GetAllLessons();
+        return Results.Ok(mapper.Map<List<LessonReadDto>>(lessons));
+    }
+
+    internal async Task<IResult> GetLessonById(ILessonRepo repo, IMapper mapper, int lsid)
+    {
+        var Lesson = await repo.GetLessonById(lsid);
+        if (Lesson == null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(mapper.Map<LessonReadDto>(Lesson));
+    }
+
+    internal async Task<IResult> UpdateLesson(ILessonRepo repo, IMapper mapper, int lsid, LessonUpdateDto lesson)
+    {
+        var lessonModel = await repo.GetLessonById(lsid);
+        if (lessonModel == null)
+        {
+            return Results.NotFound();
+        }
+
+        var ValidationResults = lesson.Validate(new ValidationContext(lesson))
+            .Select(er => er.ErrorMessage)
+            .ToList();
+
+        if (ValidationResults.Count != 0)
+        {
+            var builder = new StringBuilder();
+            foreach (var error in ValidationResults)
+            {
+                builder.Append(error);
+                builder.Append("/n");
+            }
+        }
+
+        if (lesson.Day != null)
+        {
+            lessonModel.Day = lesson.Day;
+        }
+
+        lessonModel.Hours = lesson.Hours;
+
+        lessonModel.Minutes = lesson.Minutes;
+
+        if (lesson.Time != null)
+        {
+            lessonModel.Time = lesson.Time;
+        }
+
+        await repo.SaveChanges();
+
+        var result = mapper.Map<LessonReadDto>(lessonModel);
+        return Results.Ok(result);
+
+    }
+
+    internal async Task<IResult> DeleteLesson(int lsid, ILessonRepo repo)
+    {
+        var Lesson = await repo.GetLessonById(lsid);
+        if (Lesson == null)
+        {
+            return Results.NotFound();
+        }
+        
+        repo.DeleteLesson(Lesson);
+        return Results.NoContent();
+    }
     
     public void DefineEndpoints(WebApplication app)
     {
         app.MapPost("/lessons", CreateLesson);
+        app.MapGet("/lessons", GetAllLessons);
+        app.MapGet("/lessons/{lsid}", GetLessonById);
+        app.MapPut("/lessons/{lsid}", UpdateLesson);
+        app.MapDelete("/lessons/{lsid}", DeleteLesson);
     }
 }
